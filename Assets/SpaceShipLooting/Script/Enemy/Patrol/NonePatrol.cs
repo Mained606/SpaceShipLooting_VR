e46e687e1b07f34ba7851df5b8e02e5d05aff36c
DESKTOP-52351T8\Admin <bf980326@gmail.com>
2024-12-13 12:38:06 +0900

    <Enemy> 이벤트 타입 Pipe Explode 외 1층 Enemy 용 Dispatch Event 추가 구현 - 이벤트 활성화시, 지정한 곳으로 이동해서 LookAround 1회 수행, 이후 다시 스폰됐던 포지션으로 돌아감 - 2F 배관 폭발 이벤트는 해당 위치로 가서 무한 LookAround 수행 - 1F 문제 틀리는 이벤트는 해당 위치로 가서 1회 LookAround후 회귀

--- Assets/SpaceShipLooting/Script/Enemy/Patrol/NonePatrol.cs
+++ Assets/SpaceShipLooting/Script/Enemy/Patrol/NonePatrol.cs
@@ -3,6 +3,10 @@ using UnityEngine.AI;
 
 public class NonePatrol : IEnemyPatrol
 {
+    private Vector3 spawnPosition;
+    private Vector3 currentPosition;
+    private float distance;
+    private Animator animator;
     public void Initialize(EnemyPatrol _enemyPatrol)
     {
         if (_enemyPatrol == null)
@@ -10,6 +14,10 @@ public class NonePatrol : IEnemyPatrol
             Debug.Log("EnemyPatrol이 null");
             return;
         }
+
+        spawnPosition = _enemyPatrol.spawnPosition;
+        currentPosition = _enemyPatrol.transform.position;
+        animator = _enemyPatrol.animator;
     }
 
     public bool Patrol(NavMeshAgent agent)
@@ -19,7 +27,20 @@ public class NonePatrol : IEnemyPatrol
             Debug.LogWarning("Patrol 함수에서 agent가 null입니다.");
             return false;
         }
-
+        Return(agent);
         return false;
+    }
+
+    public void Return(NavMeshAgent agent)
+    {
+        currentPosition = agent.transform.position;
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        {
+            distance = Vector3.Distance(currentPosition, spawnPosition);
+            if (distance >= agent.remainingDistance)
+            {
+                agent.SetDestination(spawnPosition);
+            }
+        }
     }
 }
