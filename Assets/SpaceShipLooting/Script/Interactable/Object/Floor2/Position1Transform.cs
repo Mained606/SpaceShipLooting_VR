d1af1c2ae36ba603ee82adc998bee1a31e27afd7
UnderBridgeKid97 <waktaverse24@gmail.com>
2024-12-18 14:29:11 +0900

    악

--- Assets/SpaceShipLooting/Script/Interactable/Object/Floor2/Position1Transform.cs
+++ Assets/SpaceShipLooting/Script/Interactable/Object/Floor2/Position1Transform.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Position1Transform : MonoBehaviour
 {
     private GameObject player;
 
-    private void Start()
+    private void OnEnable()
+    {
+        // 씬 로드 이벤트에 메서드 등록
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        // 씬 로드 이벤트에서 메서드 제거 (리소스 정리)
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    // 씬 로드가 완료되었을 때 호출
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         // 씬에서 플레이어 찾기
         player = GameObject.FindGameObjectWithTag("Player");
