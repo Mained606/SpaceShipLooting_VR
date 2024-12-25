224d304b0ec26876f02e35e6eb18933a1eafab7e
UnderBridgeKid97 <wanktaverse24@gamil.com>
2024-12-26 00:34:13 +0900

    제이슨매니저

--- Assets/SpaceShipLooting/Script/Json/TextManagerJsonData.cs
+++ Assets/SpaceShipLooting/Script/Json/TextManagerJsonData.cs
@@ -1,9 +1,7 @@
-
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Newtonsoft.Json;
-using System.Linq;
+using Newtonsoft.Json.Serialization;
+using UnityEngine;
 
 public class TextManagerJsonData
 {
@@ -12,27 +10,46 @@ public class TextManagerJsonData
 
     public static TextManagerJsonData GetInstance()
     {
-        if (TextManagerJsonData.instance == null)
-            TextManagerJsonData.instance = new TextManagerJsonData();
-        return TextManagerJsonData.instance;
+        if (instance == null)
+        {
+            instance = new TextManagerJsonData();
+        }
+        return instance;
     }
+
     public void LoadDatas()
     {
-        var String_Table = Resources.Load<TextAsset>("Json/String_Table").text;
-
-
-
-        var arr_String_DataTable = JsonConvert.DeserializeObject<String_Table[]>(String_Table);
-
-        /* foreach(var data in arrSkull_DataTable)
+        var stringTableText = Resources.Load<TextAsset>("Json/String_Table")?.text;
+        if (stringTableText == null)
         {
-            Debug.LogFormat("{0} ",data.skull_index);
-        } */
-        this.dicString_Table = arr_String_DataTable.ToDictionary(x => x.string_index);
+            Debug.LogError("String_Table.json 파일을 찾을 수 없습니다.");
+            return;
+        }
 
+        var settings = new JsonSerializerSettings
+        {
+            ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
+            ContractResolver = new DefaultContractResolver
+            {
+                DefaultMembersSearchFlags = System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance
+            },
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore // IL2CPP 호환성
+        };
 
+        try
+        {
+            var arrStringDataTable = JsonConvert.DeserializeObject<String_Table[]>(stringTableText, settings);
+            dicString_Table = new Dictionary<string, String_Table>();
 
+            foreach (var entry in arrStringDataTable)
+            {
+                dicString_Table[entry.string_index] = entry;
+            }
+            Debug.Log("JSON 데이터 로드 성공!");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError($"JSON 파싱 오류: {ex.Message}");
+        }
     }
 }
-
-
