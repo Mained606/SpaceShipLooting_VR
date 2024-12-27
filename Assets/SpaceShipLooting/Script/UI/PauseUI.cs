fa93bd2e51b8bc65b94b869518be91655bf94915
DESKTOP-52351T8\Admin <anesk120@naver.com>
2024-12-27 11:32:12 +0900

    no message

--- Assets/SpaceShipLooting/Script/UI/PauseUI.cs
+++ Assets/SpaceShipLooting/Script/UI/PauseUI.cs
@@ -12,6 +12,8 @@ public class PauseUI : MonoBehaviour
     public Transform head;
     [SerializeField] private float distance = 1.5f;
 
+    private bool isPaused = false;
+
     private void Update()
     {
         if (showButton.action.WasPressedThisFrame())
@@ -22,20 +24,32 @@ public class PauseUI : MonoBehaviour
 
     void Toggle()
     {
-        gameMenu.SetActive(!gameMenu.activeSelf);
+        isPaused = !isPaused;
+        gameMenu.SetActive(isPaused);
 
         //show 설정
-        if (gameMenu.activeSelf)
+        if (isPaused)
         {
+            //머리따라서
             gameMenu.transform.position = head.position + new Vector3(head.forward.x, 0f, head.forward.z).normalized * distance;
             gameMenu.transform.LookAt(new Vector3(head.position.x, gameMenu.transform.position.y, head.position.z));
             gameMenu.transform.forward *= -1;
+
+            //게임 멈춤
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            //게임 재개
+            Time.timeScale = 1f;
         }
     }
 
     //나가기
     public void Quit()
     {
+        Time.timeScale = 1f;
+
 #if UNITY_EDITOR
         // 에디터에서 실행 중일 경우 에디터를 중지
         UnityEditor.EditorApplication.isPlaying = false;
